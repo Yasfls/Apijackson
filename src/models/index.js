007aad37cd0ac1aa2aa5c3c17f8c0dae78a0a514
Yasmin Friedemann <yasminflsilva@gmail.com>
2025-06-15 01:16:21 -0300

    ajustado documento config para .js e atualizado index

--- src/models/index.js
+++ src/models/index.js
@@ -1,43 +1,48 @@
-// models/index.js
-import { readdirSync } from 'fs';
-import path from 'path';
-import { fileURLToPath, pathToFileURL } from 'url';
 import { Sequelize, DataTypes } from 'sequelize';
-import { readFile } from 'fs/promises';
+import dbConfig from '../config/config.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import UserModel from './user.js';
+import CategoryModel from './category.js';
+import ProductModel from './product.js';
+import OrderModel from './order.js';
+import OrderProductModel from './orderProduct.js';
 
-const configPath = path.join(__dirname, '../config/config.json');
-const configData = JSON.parse(await readFile(pathToFileURL(configPath)));
-const env = process.env.NODE_ENV || 'development';
-const config = configData[env];
+const sequelize = new Sequelize(
+  dbConfig.development.database,
+  dbConfig.development.username,
+  dbConfig.development.password,
+  {
+    host: dbConfig.development.host,
+    dialect: dbConfig.development.dialect,
+    operatorsAliases: false,
+  }
+);
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+// Testando a conexão com o banco
+try {
+  await sequelize.authenticate();
+  console.log('Conectado com o Banco de Dados.');
+} catch (err) {
+  console.error('Não foi possível conectar ao banco de dados:', err);
 }
 
 const db = {};
-const files = readdirSync(__dirname).filter(file =>
-  file.endsWith('.js') && file !== path.basename(__filename)
-);
 
-for (const file of files) {
-  const { default: modelFn } = await import(pathToFileURL(path.join(__dirname, file)).href);
-  const model = modelFn(sequelize, DataTypes);
-  db[model.name] = model;
-}
-
-for (const modelName of Object.keys(db)) {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-}
-
-db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.user = UserModel(sequelize, DataTypes);
+db.category = CategoryModel(sequelize, DataTypes);
+db.product = ProductModel(sequelize, DataTypes);
+db.order = OrderModel(sequelize, DataTypes);
+db.orderProduct = OrderProductModel(sequelize, DataTypes);
+
+
+try {
+  await sequelize.sync({ force: false });
+  console.log('Tabelas sincronizadas.');
+} catch (err) {
+  console.error('Erro ao sincronizar as tabelas:', err);
+}
 
 export default db;
